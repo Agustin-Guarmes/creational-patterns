0835677bc17a54851577ae46a83be1e16b00359b
SimonS89 <simonrsueldo@gmail.com>
2023-07-12 16:10:42 -0300

    subiendo ejemplo singleton

--- creational-patterns/src/creational/patterns/singleton/App.java
+++ creational-patterns/src/creational/patterns/singleton/App.java
@@ -1,6 +1,6 @@
 package creational.patterns.singleton;
 
-public class Singleton {
+public class App {
     public static void main(String[] args) {
         ConexionBd conexionBd = ConexionBd.getInstance();
         System.out.println(conexionBd.conectar());
